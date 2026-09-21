bd01dcc020e751867471c04ea7397c06730d85a9
migoselo <putridheerie@gmail.com>
2026-09-21 16:28:00 +0700

    gaji

--- upsend_web/vite.config.js
+++ upsend_web/vite.config.js
@@ -15,13 +15,4 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://dipodic-burlily-roxie.ngrok-free.dev',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
 })
